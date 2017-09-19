6c77e3f9768f7f9fe0c55e0cba684c1faff025e9
Jorge-SPC\Jorge <jorge.marrero106@alu.ulpgc.es>
2017-09-19 19:19:51 +0100

    Optimaze images

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const image = require('gulp-image');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
@@ -7,7 +8,13 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./assets/css/'));
 });
 
+gulp.task('image', function () {
+    gulp.src('./images/**/*')
+      .pipe(image())
+      .pipe(gulp.dest('./assets/img'));
+  });
+
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('sass/**/*.scss',['styles', 'image']);
 });
