731910d1a46350839444caae01498eeadd59bcf3
Jorge-SPC\Jorge <jorge.marrero106@alu.ulpgc.es>
2017-11-29 20:21:26 +0000

    Minify JS and CSS for homepage

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 const image = require('gulp-image');
+const rename = require('gulp-rename');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./assets/css/'));
 });
+
+gulp.task('minify-css', () => {
+    return gulp.src('assets/css/style.css')
+      .pipe(cleanCSS())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('assets/css/'));
+  });
 
 gulp.task('image', function () {
     gulp.src('./images/**/*')
@@ -16,5 +27,5 @@ gulp.task('image', function () {
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('sass/**/*.scss',['styles', 'minify-css']);
 });
