bbba8188dd86dd5a179a045d9bdde99e009fbd05
Jorge-SPC\Jorge <jorge.marrero106@alu.ulpgc.es>
2017-12-12 11:49:38 +0000

    Optimizar para SEO

---
+++ assets/js/estudio.js
@@ -0,0 +1,14 @@
+(function ($, window, document, undefined) {
+    'use strict';
+	jQuery(document).ready(function(){
+        
+    });
+
+    $(window).load(function(){
+    /* ==============================================
+        PAGE PRELOADER
+    =============================================== */
+        jQuery("#preloader").delay(600).fadeOut(500); 
+        
+    });
+})(jQuery, window, document);
